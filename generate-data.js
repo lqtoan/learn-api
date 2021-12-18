24598c3f21610f66a73fa6c0fd903df135f15ea5
lqtoan <lqtoan37@gmail.com>
2021-12-18 11:52:44 +0700

    update

--- generate-data.js
+++ generate-data.js
@@ -24,7 +24,7 @@ const randomProductList = (categoryList) => {
   const productList = [];
 
   for (const category of categoryList) {
-    let numberOfProducts = Math.floor(Math.random() * 1000);
+    let numberOfProducts = Math.floor(Math.random() * 10000);
     Array.from(new Array(numberOfProducts)).forEach(() => {
       const product = {
         categoryId: category.id,
@@ -44,7 +44,7 @@ const randomProductList = (categoryList) => {
 
 (() => {
   // random data
-  const categoryList = randomCategoryList(Math.ceil(Math.random() * 10) + 4);
+  const categoryList = randomCategoryList(Math.ceil(Math.random() * 60) + 40);
   const productList = randomProductList(categoryList);
 
   // prepare db object
