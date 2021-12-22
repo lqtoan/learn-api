f2b5fba9c771456aa096d3eeab4d772cac375a03
lqtoan <lqtoan37@gmail.com>
2021-12-22 15:34:22 +0700

    update

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ router.render = (req, res) => {
 server.use('/api', router);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000 || 5000;
 server.listen(PORT, () => {
   console.log('JSON Server is running');
 });
